5fca8848ce70a8395b9b34c605a618214b971b52
Afola-Ode <36237399+Afola-Ode@users.noreply.github.com>
2021-01-29 12:26:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  //Regular Expression is used to remove unwanted caharacters
+  var regEx = /[^A-Za-z0-9]/g;
+
+  //Convert str to lowercase & apply Regex
+  var convStr = str.toLowerCase().replace(regEx, '');
+  
+  //Reverse str
+  var revStr = convStr.split('').reverse().join('');
+
+  //Results
+  return convStr === revStr;
 }
 
 function solution(arg) {
